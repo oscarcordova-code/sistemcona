6f178d6ed78f837d5cab2d0d5855042cca8cbead
oscarcordova-code <oscar.cordova.sv.gdo@gmail.com>
2025-11-09 23:46:25 -0600

    Docente del modulo

---
+++ docentesymaterias.js
@@ -0,0 +1,37 @@
+const docentesYMaterias = [
+    {
+        docente: "Martínez Pérez Liliana Guadalupe",
+        materias: [
+            "Análisis de criptografía y seguridad",
+            "Ciberseguridad en aplicaciones WEB"
+        ]
+    },
+    {
+        docente: "Ortiz González Carolina",
+        materias: ["Conciencia histórica II: Siglos XIX y XX"]
+    },
+    {
+        docente: "Toribio Torres Ariadna",
+        materias: ["Descripción de organismos biológicos y procesos evolutivos"]
+    },
+    {
+        docente: "Mendoza Palacios César",
+        materias: ["Formación socioemocional V"]
+    },
+    {
+        docente: null,
+        materias: ["Interacción especializada en inglés"]
+    },
+    {
+        docente: "García López Patricia",
+        materias: ["Reconocimiento de patrones"]
+    },
+    {
+        docente: "Pérez Denicia Edgar",
+        materias: ["Temas selectos de Matemáticas II"]
+    },
+    {
+        docente: "Miranda Quiroz Manuel",
+        materias: ["Uso de herramientas de explotación de datos"]
+    }
+];
